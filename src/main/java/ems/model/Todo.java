b6632d8512db13bdaa5eb69417dca00bb72b48ff
tvdinh <tvdinh@tma.com.vn>
2017-09-04 19:13:15 +0700

    Add Todo model class

---
+++ src/main/java/ems/model/Todo.java
@@ -0,0 +1,93 @@
+package ems.model;
+
+import java.util.Date;
+
+public class Todo {
+    private int id;
+    private String desc;
+    private String user;
+    private Date targetDate;
+    private boolean isDone;
+
+    public Todo() {
+    }
+
+    public Todo(int id, String desc, String user, Date targetDate, boolean isDone) {
+        super();
+        this.id = id;
+        this.desc = desc;
+        this.user = user;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Todo other = (Todo) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id + ", desc=" + desc + ", user=" + user + ", targetDate=" + targetDate + ", isDone="
+                + isDone + "]";
+    }
+
+    
+}
